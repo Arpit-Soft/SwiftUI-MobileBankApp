3bd4ad0f83bfc2b4677ee0608b0af44431f959e9
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-31 22:29:15 +0530

    Initial Commit

---
+++ SwiftUI-MobileBankApp/SwiftUI_MobileBankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MobileBankAppApp.swift
+//  SwiftUI-MobileBankApp
+//
+//  Created by Arpit Dixit on 31/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MobileBankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
